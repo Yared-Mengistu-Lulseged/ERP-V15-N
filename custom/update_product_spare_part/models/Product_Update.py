089c5717ee4e8fc6f4149ce3a71cd976d7e39dc2
yohanneskass <yohanneskassahun12@gmail.com>
2024-04-21 16:14:28 +0300

    Product update for Debi spare part

--- custom/update_product_spare_part/models/Product_Update.py
+++ custom/update_product_spare_part/models/Product_Update.py
@@ -13,35 +13,27 @@ class ProductUpdate(models.Model):
     fob_etb = fields.Float(string='FOB Unit Price(ETB)',  compute='_compute_list_price')
     freight_cost_usd = fields.Float(string='Freight cost($)')
     freight_cost_etb = fields.Float(string='Freight cost (ETB)',  compute='_compute_list_price')
-    total_tax = fields.Float(string='Total Tax($) ', compute='_compute_list_price')
-    total_tax_etb = fields.Float(string='Total Tax(ETB) ', compute='_compute_list_price')
-    standard_price_usd = fields.Float(string='Cost($)', compute='_compute_list_price')
-    standard_price = fields.Float(string='Cost(ETB)', compute='_compute_list_price')
-    profit_usd = fields.Float(string='Profit($)', compute='_compute_list_price')
-    profit = fields.Float(string='Profit(ETB)', compute='_compute_list_price')
+    total_tax_etb = fields.Float(string='Total Tax(ETB) ')
+    standard_price_etb = fields.Float(string='Total Cost(ETB)', compute='_compute_list_price')
+    profit = fields.Float(string='Profit(ETB)')
     exchange_rate = fields.Float(string='Exchange Rate(ETB)', default=1)
     sales_price_etb = fields.Float(string='Sales Price(ETB)', compute='_compute_list_price')
+    other_cost = fields.Float(string='Other Cost (ETB)',)
+    direct_cost = fields.Float(string='Direct Cost (ETB)', compute='_compute_list_price')
+    admin_cost = fields.Float(string='Admin(Indirect Cost) (ETB)')
 
-    @api.onchange('fob_etb', 'fob_usd', 'freight_cost_usd', 'freight_cost_etb', 'exchange_rate')
+
+    @api.onchange('fob_etb', 'fob_usd', 'freight_cost_usd', 'freight_cost_etb', 'exchange_rate', 'total_tax_etb', 'admin_cost', 'direct_cost', 'profit')
     def _compute_list_price(self):
         for product in self:
-            if product.fob_usd and product.freight_cost_usd and product.exchange_rate:
-                product.total_tax = 0.75 * (product.fob_usd + product.freight_cost_usd)
+            if product.fob_usd and product.freight_cost_usd and product.exchange_rate and product.total_tax_etb:
                 product.fob_etb = product.fob_usd * product.exchange_rate
                 product.freight_cost_etb = product.freight_cost_usd * product.exchange_rate
-                product.total_tax_etb = 0.75 * ((product.fob_usd * product.exchange_rate) + (product.freight_cost_usd * product.exchange_rate))
-                product.standard_price_usd = product.fob_usd + product.freight_cost_usd + product.total_tax
-                product.profit_usd = 0.3 * product.standard_price_usd
-                product.sales_price_etb = product.profit + product.standard_price
-
-            if product.fob_etb and product.freight_cost_etb:
-                product.total_tax_etb = 0.75 * (product.fob_etb + product.freight_cost_etb)
-                product.standard_price = product.fob_etb + product.freight_cost_etb + product.total_tax_etb
-                product.profit = 0.3 * product.standard_price
+                product.direct_cost = product.fob_etb + product.freight_cost_etb + product.other_cost + product.total_tax_etb
+                product.standard_price_etb = product.direct_cost + product.admin_cost
+                product.sales_price_etb = product.profit + product.standard_price_etb
 
             else:
-                product.total_tax = product.total_tax
-                product.standard_price = product.standard_price
                 product.profit = product.profit
                 product.exchange_rate = product.exchange_rate
                 product.sales_price_etb = product.sales_price_etb
@@ -50,8 +42,10 @@ class ProductUpdate(models.Model):
                 product.fob_usd = product.fob_usd
                 product.freight_cost_usd = product.freight_cost_usd
                 product.total_tax_etb = product.total_tax_etb
-                product.standard_price_usd = product.standard_price_usd
-                product.profit_usd = product.profit_usd
+                product.standard_price_etb = product.standard_price_etb
+                product.other_cost = product.other_cost
+                product.direct_cost = product.direct_cost
+                product.admin_cost = product.admin_cost
 
 
 
